f42fe007c21503ef12adbd294175c75d23843666
Walter Gomes <wg966@hotmail.com>
2018-06-07 19:05:40 -0300

    Started from rest_framework boilerplate. Added call record model.

---
+++ olistphone/rest/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+# CallRecord models the start and end of a phone call
+class CallRecord(models.Model):
+    # Call records can be either Start or End records
+    # Enumeration makes further modifications, if necessary, easier
+    RECORD_TYPES = (
+        ('S', 'Start'),
+        ('E', 'End'),
+    )
+    # Type of the record model
+    record_type = models.CharField(max_length=1, choices=RECORD_TYPES, required=True)
+    
+    # Timestamp of the event
+    record_timestamp = models.DateTimeField(auto_now_add=True, required=True)
+    
+    # Unique-pair call ID.
+    call_id = models.CharField(max_length=255, required=True)
+    # Since call_id is not unique, but a unique-pair, we need to define a combination of fields for uniqueness
+    class Meta:
+        # This makes it so we can have one Start record and one End record with the same call_id, but never more than one
+        unique_together = ['record_type', 'call_id']
+
+    # Here we define what a valid phone number is, following the repo's guidelines
+    # Phone numbers have to be all digits (0-9)
+    # Their length has to be between 10 (2 area digits + 8 phone digits) and 11 (2 area digits + 9 phone digits)
+    phone_validator_regex = RegexValidator(regex=r'^\d{10,11}', message='Phone numbers must be all digits, with 2 area code digits and 8 or 9 phone number digits.')
+    # Source (caller) phone number. Uses phone_validator_regex for validation
+    source_number = models.CharField(validators=[phone_validator_regex], max_length=11)
+    # Destination (called) phone number. Uses phone_validator_regex for validation
+    destination_number = models.CharField(validators=[phone_validator_regex], max_length=11)
